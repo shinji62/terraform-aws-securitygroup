575bf5ada69019d621b11588648f01ed6d6665f0
Etourneau Gwenn <gwenn.etourneau@gmail.com>
2019-09-05 14:38:49 +0900

    first commit

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "vpc_id" {}
+variable "http_port" {
+  default = "8500"
+}
+variable "https_port" {
+  default = "8501"
+}
+variable "grpc_port" {
+  default = "8502"
+}
+variable "cluster_rpc_port" {
+  default = "8600"
+}
+variable "lan_serf" {
+  default = "8301"
+}
+variable "cidr_blocks" {
+  type     = list(string)
+  default = ["0.0.0.0/0"]
+}
